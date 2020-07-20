a814fad455a1624e9cb3a870501e08821062bc9b
David Ruiz <davidl.ruizc@gmail.com>
2020-07-20 01:29:27 -0500

    List users with cognito, bug on aws credentials

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module dockergo
 go 1.14
 
 require (
+	github.com/aws/aws-sdk-go v1.33.7 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
--- main.go
+++ main.go
@@ -2,12 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
+// App holds internals for auth flow.
+type App struct {
+	CognitoClient *cognito.CognitoIdentityProvider
+	UserPoolID    string
+	AppClientID   string
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
+	// env variables declaration
+	UserPoolID := os.Getenv("USER_POOL_ID")
+	//AppClientID := os.Getenv("APP_CLIENT_ID")
+	// fmt.Println(os.Getenv("AWS_ACCESS_KEY_ID"))
+
+	// try connection with User pool
+	conf := &aws.Config{Region: aws.String("us-east-1")}
+	sess, err := session.NewSession(conf)
+	if err != nil {
+		fmt.Println("Failed to create session: ", err)
+		return
+	}
+
+	svc := cognito.New(sess)
+
+	params := &cognito.ListUsersInput{
+		UserPoolId: aws.String(UserPoolID),
+		AttributesToGet: []*string{
+			aws.String("email"),
+		},
+	}
+
+	resp, err := svc.ListUsers(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(resp)
+
+	// echo request
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
