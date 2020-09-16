e98b89575e876e55d857d1bf66cee1981196dcc3
davidlruizc <davidl-ruizc@outlook.com>
2020-09-16 14:52:09 -0500

    command for export json

---
+++ cmd/exportPool.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"dockergo/app"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+	"github.com/joho/godotenv"
+)
+
+// exportCmd represents the export pool command
+var exportCmd = &cobra.Command{
+	Use:   "export",
+	Short: "export a JSON of cognito userpool",
+	Long:  `export allows you to generate a JSON file with all of user pools signed on your AWS cognito user pool.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		exportPoolCognito()
+		fmt.Println("Your user pool has been exported successfully as user-pool-output.json")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(exportCmd)
+	// exportCmd.Flags().BoolP("float", "f", false, "Add Floating Numbers")
+}
+
+func exportPoolCognito() {
+	// dot env initial config.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
+	region := os.Getenv("AWS_REGION")
+
+	// creating aws session
+	conf := &aws.Config{Region: aws.String(region)}
+	sess, err := session.NewSession(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// App definition
+	cli := app.App{
+		CognitoClient: cognito.New(sess),
+		UserPoolID:    os.Getenv("USER_POOL_ID"),
+		AppClientID:   os.Getenv("APP_CLIENT_ID"),
+	}
+
+	users := cli.CognitoUserPoolPaginated().Users
+
+	// export data in JSON file
+	rankingsJSON, _ := json.Marshal(users)
+	err = ioutil.WriteFile("user-pool-output.json", rankingsJSON, 0644)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,22 +4,60 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "poolcognito",
-	Short: "Poolcognito it's a cognito user pool export CLI",
-	Long:  `A very useful way to export your users pool from AWS cognito user pool service.`,
-	Run: func(cmd *cobra.Command, arg []string) {
-		fmt.Println("TEST command")
-	},
-}
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "poolcognito",
+		Short: "Poolcognito it's a cognito user pool export CLI",
+		Long:  `A very useful way to export your users pool from AWS cognito user pool service.`,
+		Run: func(cmd *cobra.Command, arg []string) {
+			fmt.Println("Welcome to PoolCognito CLI")
+		},
+	}
+)
 
 // Execute executes the root command.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.userpool-connection.yaml)")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".userpool-connection" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".userpool-connection")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
