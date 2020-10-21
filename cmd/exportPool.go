81b0f028d914619d42b40499cdb0de3a79813540
David Ruiz <davidl-ruizc@outlook.com>
2020-10-20 23:08:04 -0500

    adding app id, pool id and region flags to export

--- cmd/exportPool.go
+++ cmd/exportPool.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 
 	"github.com/spf13/cobra"
 
@@ -16,30 +15,40 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var awsRegion string
+var userPoolID string
+var appClientID string
+
 // exportCmd represents the export pool command
 var exportCmd = &cobra.Command{
 	Use:   "export",
 	Short: "export a JSON of cognito userpool",
 	Long:  `export allows you to generate a JSON file with all of user pools signed on your AWS cognito user pool.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		exportPoolCognito()
-		fmt.Println("Your user pool has been exported successfully as user-pool-output.json")
+		if userPoolID != "" && appClientID != "" && awsRegion != "" {
+			exportPoolCognito(userPoolID, appClientID, awsRegion)
+		} else {
+			fmt.Println("Sorry, you must specify the user pool id and the app client id like this:")
+			fmt.Println("poolcognito export --region us-east-1 --poolid <user-pool-id> --clientid <app-client-id>")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(exportCmd)
-	exportCmd.Flags().BoolP("float", "f", false, "Add Floating Numbers")
+	exportCmd.Flags().StringVar(&awsRegion, "region", "", "AWS region (required)")
+	exportCmd.Flags().StringVar(&userPoolID, "poolid", "", "AWS cognito user pool id (required)")
+	exportCmd.Flags().StringVar(&appClientID, "clientid", "", "AWS cognito app client id (required)")
 }
 
-func exportPoolCognito() {
+func exportPoolCognito(userPoolID string, appClientID string, awsRegion string) {
 	// dot env initial config.
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file.")
 	}
 
-	region := os.Getenv("AWS_REGION")
+	region := awsRegion
 
 	// creating aws session
 	conf := &aws.Config{Region: aws.String(region)}
@@ -51,13 +60,18 @@ func exportPoolCognito() {
 	// App definition
 	cli := app.App{
 		CognitoClient: cognito.New(sess),
-		UserPoolID:    os.Getenv("USER_POOL_ID"),
-		AppClientID:   os.Getenv("APP_CLIENT_ID"),
+		UserPoolID:    userPoolID,
+		AppClientID:   appClientID,
 	}
 
-	users := cli.CognitoUserPoolPaginated().Users
+	users, err := cli.CognitoUserPoolPaginated()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// export data in JSON file
-	rankingsJSON, _ := json.Marshal(users)
-	err = ioutil.WriteFile("user-pool-output.json", rankingsJSON, 0644)
+	userPoolJSON, _ := json.Marshal(users.Users)
+	err = ioutil.WriteFile("user-pool-output.json", userPoolJSON, 0644)
+	fmt.Println("Your user pool has been exported successfully as user-pool-output.json")
 }
